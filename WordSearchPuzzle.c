4a8a27f339f82647266340b99161476070a771ff
annatyk <61871337+annatyk@users.noreply.github.com>
2020-03-06 19:41:18 +0800

    Update WordSearchPuzzle.c

--- WordSearchPuzzle.c
+++ WordSearchPuzzle.c
@@ -1,26 +1,3 @@
-/**
- * ENGG1110 Problem Solving by Programming
- *
- * Course Project
- *
- * I declare that the project here submitted is original
- * except for source material explicitly acknowledged,
- * and that the same or closely related material has not been
- * previously submitted for another course.
- * I also acknowledge that I am aware of University policy and
- * regulations on honesty in academic work, and of the disciplinary
- * guidelines and procedures applicable to breaches of such
- * policy and regulations, as contained in the website.
- *
- * University Guideline on Academic Honesty:
- *   https://www.cuhk.edu.hk/policy/academichonesty/
- *
- * Student Name  : Tam Yi Ki <your name>
- * Student ID    : 1155126127 <your student ID>
- * Class/Section : ENGG1110 D <your class/section>
- * Date          : 02/11/2018<date>
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
